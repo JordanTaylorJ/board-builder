b20cefafd5d2c4f9be1739ebd6bee8efff05a8b8
JordanJoseph <jordant.joseph@outlook.com>
2022-05-21 13:57:37 -0600

    add dbjson

--- src/components/BoardBuilder.jsx
+++ src/components/BoardBuilder.jsx
@@ -10,18 +10,32 @@ import Typography from '@mui/material/Typography';
 
 const steps = [
   {
-    label: 'Select campaign settings',
+    label: 'Select a deck',
     description: `For each ad campaign that you create, you can control how much
               you're willing to spend on clicks and conversions, which networks
               and geographical locations you want your ads to show on, and more.`,
   },
   {
-    label: 'Create an ad group',
+    label: 'Choose your trucks',
     description:
       'An ad group contains one or more ads which target a shared set of keywords.',
   },
   {
-    label: 'Create an ad',
+    label: 'Select your wheels',
+    description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+  },
+  {
+    label: 'Add risers',
+    description: `Try out different ad text to see what brings in the most customers,
+              and learn how to enhance your ads using features like ad extensions.
+              If you run into any problems with your ads, find out how to tell if
+              they're running and how to resolve approval issues.`,
+  },
+  {
+    label: 'Grip tape',
     description: `Try out different ad text to see what brings in the most customers,
               and learn how to enhance your ads using features like ad extensions.
               If you run into any problems with your ads, find out how to tell if
