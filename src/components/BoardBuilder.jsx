c99f429d2d7c361080c5d1866a4c38ef70088589
JordanJoseph <jordant.joseph@outlook.com>
2022-05-21 14:07:00 -0600

    add selection dropdown component

--- src/components/BoardBuilder.jsx
+++ src/components/BoardBuilder.jsx
@@ -7,6 +7,7 @@ import StepContent from '@mui/material/StepContent';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import Selector from './Selector';
 
 const steps = [
   {
@@ -75,6 +76,7 @@ const BoardBuilder =() => {
             <StepContent>
               <Typography>{step.description}</Typography>
               <Box sx={{ mb: 2 }}>
+                  <Selector />
                 <div>
                   <Button
                     variant="contained"
