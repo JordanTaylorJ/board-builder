9c88e7a47fdca5b19f67d1a25c0689adbb3f7a28
JordanJoseph <jordant.joseph@outlook.com>
2022-05-31 16:36:01 -0600

    reformat BoardBuilder component without the MUI stepper

--- src/components/BoardBuilder.jsx
+++ src/components/BoardBuilder.jsx
@@ -1,91 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import StepContent from '@mui/material/StepContent';
-import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Selector from './Selector'; 
+import React, { useState, useEffect } from 'react';
 
-const BoardBuilder =({boards, handleAddBoard}) => {
-  const [activeStep, setActiveStep] = React.useState(0);
+const BoardBuilder = () => {
 
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
-  const [steps, setSteps] = useState([]);
-
+  //const [newBoard, setNewBoard] = useState([]);
+  const [steps, setSteps] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:3001/steps")
     .then(r => r.json())
-    .then(data => setSteps(data))
-  }, []);   
+    .then((data) => setSteps(data))
+  }, []);
+
 
   return (
-    <Box sx={{ maxWidth: 400 }}>
-      <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map((step, index) => (
-          <Step key={step.label}>
-            <StepLabel
-              optional={
-                index === 4 ? (
-                  <Typography variant="caption">Last step</Typography>
-                ) : null
-              }
-            >
-              {step.label}
-            </StepLabel>
-            <StepContent>
-              <Typography>{step.description}</Typography>
-              <Box sx={{ mb: 2 }}>
-                  <Selector 
-                    parts={step.parts} 
-                    index={step.parts.index}
-                    handleAddBoard={handleAddBoard}
-                  />
-                <div>
-                  <Button
-                    variant="contained"
-                    onClick={handleNext}
-                    sx={{ mt: 1, mr: 1 }}
-                  >
-                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                  </Button>
-                  <Button
-                    disabled={index === 0}
-                    onClick={handleBack}
-                    sx={{ mt: 1, mr: 1 }}
-                  >
-                    Back
-                  </Button>
-                </div>
-              </Box>
-            </StepContent>
-          </Step>
-        ))}
-      </Stepper>
-      {activeStep === steps.length && (
-        <Paper square elevation={0} sx={{ p: 3 }}>
-          <Typography>Click on the results - you&apos;re finished</Typography>
-          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
-            Reset
-          </Button>
-        </Paper>
+    <div>
+      {steps.map((step) => 
+        <form key={step.id}> 
+          <label> {step.label}</label>
+          <input 
+            type="text" 
+            name="deck"
+            aria-label="deck"
+          ></input>
+        </form>
       )}
-    </Box>
-  );
+    </div>
+  )
 }
 
 export default BoardBuilder;
